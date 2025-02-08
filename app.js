4d404fb75cdb00f49abfae11850cd144869fd11a
AshishPradhan257 <ashish257pradhan@gmail.com>
2025-02-09 03:56:22 +0530

    upload pic

--- app.js
+++ app.js
@@ -6,6 +6,11 @@ const cookieParser = require('cookie-parser');
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const post = require('./models/post');
+const crypto = require("crypto");
+const upload = require("./config/multerconfig");
+ 
+const path = require("path");
+
 
 // app.get("/", (req, res)=>{
 //     res.send("hey");
@@ -39,10 +44,26 @@ app.set("view engine", "ejs");
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
 
+ 
 app.get('/', (req,res)=>{
     res.render("index");
 })
+
+app.get('/profile/upload', (req,res)=>{
+    res.render("profileupload");
+})
+
+app.post('/upload', isLoggedIn, upload.single("image"), async (req,res)=>{
+    let user = await userModel.findOne({email: req.user.email});
+    user.profilepic = req.file.filename;
+    await user.save();
+    res.redirect("/profile");
+
+})
+
+ 
 
 app.get('/login', (req,res)=>{
     res.render("login");
